b016bd736a232480032bc2d55cc5db24e5e74f29
Bruno Lopes / <117863700+BrunoLopes24@users.noreply.github.com>
2024-03-15 23:34:55 +0000

    Created Class Seller

--- demo-dao-jdbc/src/Main/Program.java
+++ demo-dao-jdbc/src/Main/Program.java
@@ -1,9 +1,17 @@
 package Main;
 
+import java.util.Date;
+import model.entitites.Department;
+import model.entitites.Seller;
+
 public class Program {
 
     public static void main(String[] args) {
+        Department obj = new Department(1, "Books");
         
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+                
+        System.out.println(seller);
     }
     
 }
